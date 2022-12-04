7008d9c0ef9e0db98c72f802efab115cb2730593
ColoMack <colomack6@gmail.com>
2022-12-04 16:16:18 +0300

    Sign up function.

--- src/js/userSignup.js
+++ src/js/userSignup.js
@@ -6,23 +6,45 @@ document.getElementById("submitsignupBtn").onclick = function(){
     firebase.auth().createUserWithEmailAndPassword(email, password).then((userCredential) => {
 
         let user = userCredential.user;
+        let userId = user.uid;
         let profileName = document.getElementById("newUserProfileName").value;
 
         let Bday = document.getElementById("dobDay").value;
 
         // function for getting the value of month from a select tag..
-        let selectmonth = documnet.getElementById("dobMonth");
-        function gettingmonth(){
-            let Bmonth = selectmonth.value;
-            document.getElementById("show").value = Bmonth;
+        function getmonth(){
+            let monthvalue = document.getElementById("dobMonth").value;
+            return monthvalue;       
         }
-
+        let Bmonth = getmonth();
         let BYear = document.getElementById("dobYear").value;
+        //let stringdate = Bday + "/" + Bmonth + "/" + BYear;
+
+        let dobDate = new Date(Bday + "/" + Bmonth + "/" + BYear).toDateString();
+        let signupDate = new Date();
+
+        firebase.firestore().collection("userCredentials").doc(userId).set({
+            theUserId:userId,
+            theProfilename: profileName,
+            theEmail:email,
+            theDOB:dobDate,
+            signupTimestamp: signupDate
+        }).then(() => {
+
+            window.location.href = "homepage.html";
+
+        }).catch(() => {
+
+            alert("firestore error...check code");
+
+        })
+
     
     }) .catch ((error) => {
     
         let errorcode = error.code;
         let errorMessage = error.message;
         
+        alert("auth error..check code");
     });
 }
